74c85e4896be5832cd6a219a23ebcc4920f33100
Neel Deshpande <deshpande.nee@husky.neu.edu>
2021-04-14 19:56:41 -0400

    added bath, bed and area details

--- src/main/java/com/example/wbdvsp21proprentalserverjava/models/PropertyDetails.java
+++ src/main/java/com/example/wbdvsp21proprentalserverjava/models/PropertyDetails.java
@@ -21,6 +21,10 @@ public class PropertyDetails {
     private String city;
     private String state;
     private String zipcode;
+    private int bathCount;
+    private int bedCount;
+    private double areaSqFt;
+
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @MapsId
     @JoinColumn(name = "PROPERTY_ID")
@@ -33,10 +37,14 @@ public class PropertyDetails {
         this.setZipcode("");
     }
 
-    public PropertyDetails(String city, String state, String zipcode) {
+    public PropertyDetails(String city, String state, String zipcode, int bathCount, int bedCount
+      , double areaSqFt) {
         this.city = city;
         this.state = state;
         this.zipcode = zipcode;
+        this.bathCount = bathCount;
+        this.bedCount = bedCount;
+        this.areaSqFt = areaSqFt;
     }
 
     public String getCity() {
@@ -63,6 +71,40 @@ public class PropertyDetails {
         this.zipcode = zipcode;
     }
 
+
+
+    public int getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(int propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public int getBathCount() {
+        return bathCount;
+    }
+
+    public void setBathCount(int bathCount) {
+        this.bathCount = bathCount;
+    }
+
+    public int getBedCount() {
+        return bedCount;
+    }
+
+    public void setBedCount(int bedCount) {
+        this.bedCount = bedCount;
+    }
+
+    public double getAreaSqFt() {
+        return areaSqFt;
+    }
+
+    public void setAreaSqFt(double areaSqFt) {
+        this.areaSqFt = areaSqFt;
+    }
+
     public Property getProperty() {
         return property;
     }
@@ -81,6 +123,24 @@ public class PropertyDetails {
         private String state;
         private String city;
         private String zipcode;
+        private int bathCount;
+        private int bedCount;
+        private double areaSqFt;
+
+        public PropertyDetailsBuilder setBathCount(int bathCount) {
+            this.bathCount = bathCount;
+            return this;
+        }
+
+        public PropertyDetailsBuilder setBedCount(int bedCount) {
+            this.bedCount = bedCount;
+            return this;
+        }
+
+        public PropertyDetailsBuilder setAreaSqFt(double areaSqFt) {
+            this.areaSqFt = areaSqFt;
+            return this;
+        }
 
         public PropertyDetailsBuilder setState(String state) {
             this.state = state;
@@ -98,7 +158,8 @@ public class PropertyDetails {
         }
 
         public PropertyDetails build() {
-            return new PropertyDetails(this.city, this.state, this.zipcode);
+            return new PropertyDetails(this.city, this.state, this.zipcode, this.bathCount,
+              this.bedCount, this.areaSqFt);
         }
     }
 
