04c85da716145cefe8bd05794d2c17fcf06c3503
Neel Deshpande <deshpande.nee@husky.neu.edu>
2021-04-17 11:29:26 -0400

    param syntax

--- src/main/java/com/example/wbdvsp21proprentalserverjava/repositiories/ListingRepository.java
+++ src/main/java/com/example/wbdvsp21proprentalserverjava/repositiories/ListingRepository.java
@@ -3,11 +3,12 @@ import com.example.wbdvsp21proprentalserverjava.models.Listing;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ListingRepository extends CrudRepository<Listing, Integer> {
 
     @Query(value = "select * from listings l where l.ID in (select ul.LISTING_ID from "
-      + "user_listing_lookup ul where ul.USER_ID = ?1)", nativeQuery = true)
-    List<Listing> findListingsForUser(int userId);
+      + "user_listing_lookup ul where ul.USER_ID = :userId)", nativeQuery = true)
+    List<Listing> findListingsForUser(@Param("userId") int userId);
 
 }
